398c50381a53790879cae1964a27ff012672bfd6
Anas Muhammad <43679515+anasm-17@users.noreply.github.com>
2020-01-24 16:51:38 -0800

    updated EDA.ipynb

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -1,24 +1,17 @@
-
 # author: M. Maldonado, S.Trehan, A. Muhammad
 # date: 2020-01-18
-
 '''The following scripts downloads zip files, from a determined URL , unzips them and saves into a determined file path.
 This script takes a url to the data and a 
 file location as the arguments.
-
 Usage: data_download.py --url=<url> --destination=<destination>
+
 Options:
---url=<url> The url containing the zip giles
+url=<url> The url containing the zip files
 --destination=<destination>  Path where files are to be saved
 '''
-
 import requests, zipfile, io, pytest
 from docopt import docopt
-
-
 opt = docopt(__doc__)
-
-
 def main(url, destination):
     """ 
     This function downloads , unzips and saves a .zip from a determined URL into a determined file path.
@@ -35,12 +28,10 @@ def main(url, destination):
     ---------
     None
     """
-     
     raw_data = requests.get(url)
     zip_file = zipfile.ZipFile(io.BytesIO(raw_data.content))
-    zip_file.extractall("./" + destination)
+    zip_file.extractall(destination)
     print("File(s) extracted into", destination)
-    return
-
 #Calling main function
-main(url=opt['<url>'], destination=opt['--destination'])
+if __name__ == "__main__":
+    main(url=opt['--url'], destination=opt['--destination'])
